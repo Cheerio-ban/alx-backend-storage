89b0be9dfce2b781c717402051090f58cacd7f7a
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-12 21:37:44 +0100

    Rectified long lost error

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
 -- Old school band
-SELECT band_name, (IF(split , CAST(YEAR(split) AS INT) - CAST(YEAR(formed) AS INT), 2022 - CAST(YEAR(formed)))) AS lifespan
+SELECT band_name, COALESCE(split, 2022) - formed AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'; 
