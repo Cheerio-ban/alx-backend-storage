d31676be39257d10f924103cb2c2ba8bcc08591b
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-12 21:29:41 +0100

    Glam rocks

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
 -- Old school band
-SELECT band_name, IF(split , YEAR(split) - YEAR(formed), 2022 - YEAR(formed)) AS lifespan
+SELECT band_name, (IF(split , CAST(YEAR(split) AS INT) - CAST(YEAR(formed) AS INT), 2022 - CAST(YEAR(formed)))) AS lifespan
 WHERE style = 'Glam rock'; 
