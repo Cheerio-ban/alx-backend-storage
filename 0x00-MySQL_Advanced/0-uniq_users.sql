a92aa0dfb312395483ff991e3a48e1d43e419f13
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-12 20:05:54 +0100

    Unique users SQL

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,7 +2,7 @@
 -- Creates a table users
 -- with attributes id, email, name
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255)
 );
