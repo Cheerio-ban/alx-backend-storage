30c15cf633ec823a1c884db3c9d33498bbdcc74d
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-13 09:13:11 +0100

    Rectiied possible error with default

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,7 +6,7 @@ TRIGGER check_valid_email
 AFTER UPDATE
 ON users FOR EACH ROW
 BEGIN
-    IF New.email <> Old.email Therion
+    IF New.email <> Old.email THEN
     UPDATE users SET valid_email = NULL WHERE id = New.id;
     END IF;
 END$$
