f955426eb51607a52f12d149260e1b6ebcbbc1c8
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-13 08:34:42 +0100

    Store Trigger

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,8 +1,7 @@
 -- Creates a trigger that decreases a quantity after update.
 DELIMITER $$
 DROP TRIGGER IF EXISTS reduce_item$$
-CREATE
-TRIGGER reduce_item
+CREATE TRIGGER reduce_item
 AFTER INSERT ON orders 
 FOR EACH ROW
 BEGIN
