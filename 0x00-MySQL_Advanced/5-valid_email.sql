0e801d39e759e67eeb88a8126fd2484ce8bef525
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-13 09:43:57 +0100

    Rectiied possible error with default

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,12 @@
 -- Creates a trigger and reset the value valid_email
-DELIIMITER $$
+DELIMITER $$
 DROP TRIGGER IF EXISTS check_valid_email$$
 CREATE
 TRIGGER check_valid_email
-BEFORE UPDATE ON users
-FOR EACH ROW
-IF NEW.email != OLD.email
-THEN SET NEW.valid_email = 0;
-END IF;
-$$
+AFTER UPDATE
+ON users FOR EACH ROW
+BEGIN
+    IF New.email <> Old.email THEN
+    SET New.valid_email = NULL WHERE id = New.id;
+    END IF;
+END$$
