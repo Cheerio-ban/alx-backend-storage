ea9e56adab658dad7712f0d5f7a2684b22b3846f
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-17 20:18:23 +0100

    Lists all documents in a collection using pymongo

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+Lists all documents in a collection using Pymongo
+"""
+
+
+def list_all(mongo_collection):
+    """Lists all documents in a collection"""
+    return mongo_collection.find()
