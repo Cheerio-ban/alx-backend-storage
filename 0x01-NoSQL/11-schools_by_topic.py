f636e1ebf00eb25548d57b97cfc2fbe61ac95fff
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-18 01:50:11 +0100

    Schools by topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Returns list of schools having a specific topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns schools with topic
+    """
+    return mongo_collection.find( { "topics": topic } )
