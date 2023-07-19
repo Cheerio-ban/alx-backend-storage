3546ae9a5c801a2f05da16e6fdc4168662de9625
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-19 14:24:23 +0100

    Implemented the get method and also implemented both string and int parametrize methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,7 +7,7 @@ This module contains a cache file that uses REDIS
 # imports
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -23,3 +23,17 @@ class Cache:
         rand_key = str(uuid.uuid4())
         self._redis.set(rand_key, data)
         return rand_key
+
+    def get(self, key: str, fn: Optional[Callable]) -> Union[str, bytes, int, float]:
+        """ Get method for the cache """
+        if fn:
+            return fn(self._redis.get(key))
+        return self._redis.get(key)
+    
+    def get_str(self, key: bytes) -> str:
+        """ Parametrize to string """
+        return (key).decode('utf-8')
+    
+    def get_int(self, key: bytes) -> int:
+        """ Parametrize to integer """
+        return int(key.decode('utf-8'))
