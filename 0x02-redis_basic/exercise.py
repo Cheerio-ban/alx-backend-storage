ed27e7b3f7e8d6c06dc32e69a054cadd5e933222
Chiemezie Jacob <chiemezie.jacob@gmail.com>
2023-07-19 13:31:58 +0100

    Created a Cache class using redis and implemented a store method to add key-values to the cache

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a cache file that uses REDIS
+"""
+
+# imports
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """A cache class using redis instance"""
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb
+
+    def store(self, data: Union[int, float, str, bytes]) -> str:
+        """
+            Stores an object in the redis instance using rand_key from uuid
+        """
+        rand_key = str(uuid.uuid4())
+        self._redis.set(rand_key, data)
+        return rand_key
